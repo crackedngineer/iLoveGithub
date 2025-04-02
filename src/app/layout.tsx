0ddc03afc3d0a02896413a792e104bdff856e174
Subhomoy Roy Choudhury <64763636+subhomoy-roy-choudhury@users.noreply.github.com>
2025-04-02 17:20:25 +0000

    fixed tailwind theme issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Header from '@/components/Header';
 import Footer from "@/components/Footer";
 import "./globals.css";
-import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
