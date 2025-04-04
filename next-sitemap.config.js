a5bab601ee2de469a4b58f46a24280d2fe326b70
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2025-04-05 02:00:41 +0530

    added next sitemap utility

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteUrl: 'https://ilovegithub.oderna.com',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+  exclude: [],
+  // sourceDir: 'src',
+};
