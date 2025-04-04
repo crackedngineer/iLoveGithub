d145c50c49da16adfb3a82a3b17de79d018bf2e1
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2025-04-05 02:13:40 +0530

    added deploy workflow to github pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import { NextConfig } from "next";
 import { GithubToolsList } from "@/constants";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: "/",
   images: {
+    unoptimized: true,
     remotePatterns: Object.values(GithubToolsList)
       .map((item) => {
         try {
@@ -15,7 +19,9 @@ const nextConfig: NextConfig = {
         }
       })
       .filter(
-        (pattern): pattern is { protocol: "http" | "https"; hostname: string } =>
+        (
+          pattern
+        ): pattern is { protocol: "http" | "https"; hostname: string } =>
           pattern !== null
       ), // Type-safe filtering
   },
