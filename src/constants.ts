d76275019faf24d1aa1af77d6ce75e074aca4949
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2025-05-11 15:10:08 +0530

    updated youtube demo URL

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export const RECENT_TRENDING_REPO_UI_MAXCOUNT = 4;
 
 export const RECENT_TRENDING_REPO_CACHE_MAXCOUNT = 4;
 
-export const DEMO_VIDEO_URL = "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
+export const DEMO_VIDEO_URL = "https://www.youtube.com/embed/eg5eAEnNEVY?si=_me67cuBbIloxm8o?autoplay=1"
 
 export const DefaultGithubRepo = {
   owner: "crackedngineer",
